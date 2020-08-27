bd1803e17dbe75268961a7f117f86db64b7db9a1
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-27 21:26:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,6 +13,10 @@ def link
   warning = "It's Dangerous To Go Alone! Take This."
   puts warning
 end 
-mario
-toadstool
-link
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end
+all_phrases
